815b61b7f16ffe552ca2787634091d279c9181f7
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-21 18:40:17 +0530

    Comment added to tar- to test deployment

--- script/build.sh
+++ script/build.sh
@@ -3,3 +3,4 @@ mkdir build
 mv * build/
 tar -cvzf nearby.tar.gz build/* --exclude=../script --exclude=../_git-repositories-cache --exclude=../repositoryData
 rm -r build/
+echo "Compressed tar created."
