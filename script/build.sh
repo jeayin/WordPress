24119088fe145e4068f16cae1feec8b300d98ef0
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-21 18:55:24 +0530

    Tar path updated/fix

--- script/build.sh
+++ script/build.sh
@@ -2,7 +2,7 @@ echo "Starts building php build"
 mkdir build
 mv * build/
 cd build
-tar -cvzf nearby.tar.gz * --exclude=../script --exclude=../_git-repositories-cache --exclude=../repositoryData
+tar -cvzf ../nearby.tar.gz * --exclude=../script --exclude=../_git-repositories-cache --exclude=../repositoryData
 cd ..
 rm -r build/
 echo "Compressed tar created."
