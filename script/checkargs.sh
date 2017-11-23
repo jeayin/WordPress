35e6a2d6d109e8db6f141c256d42037c82f0bd16
Kandasamy <jayaprakash.kandasamy@cgi.com>
2017-11-23 17:08:38 +0530

    build script and Bamboo argumnets finding script

---
+++ script/checkargs.sh
@@ -0,0 +1,7 @@
+echo "args 0:"$0 > script/args.txt
+echo "args 1:"$1 >> script/args.txt
+echo "args 2:"$2 >> script/args.txt
+echo "args 3:"$3 >> script/args.txt
+echo "args 4:"$4 >> script/args.txt
+echo "args 5:"$5 >> script/args.txt
+echo "args 6:"$6 >> script/args.txt
